ee46da0939e78900c8eaedcb2b6c46a340a0a4f7
Omar-Eldeeb98 <oeldeeb111@gmail.com>
2024-07-28 15:47:58 +0300

    assignment solution

---
+++ C42-G02-ADV01/Question 1/OptimizeBubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_ADV01.Question_1
+{
+    internal class OptimizeBubbleSort
+    {
+
+        public static void optimizeBubbleSort(int[] array)
+        {
+            bool flag;
+            for(int i =0; i< array.Length - 1; i ++)
+            {
+                flag = false;
+                for (int j = 0; j < array.Length - i - 1; j++ )
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                        flag = true;
+                    }
+                }
+                if(!flag)
+                {
+                    break;
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+}
